d7cedc0a495b5530a821b0a438bc46fc46b0ef40
Eli Derjabin <derjabineli@gmail.com>
2021-02-12 15:35:02 -0800

    first commit

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+const mySQL = require("mysql")
+require('dotenv').config()
+
+var connection = mySQL.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASENAME
+})
+
+connection.connect((err) => {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
---
+++ controllers/burgers_controllers.js
@@ -0,0 +1,45 @@
+const express = require("express")
+const router = express.Router();
+const burger = require("../models/burger")
+
+router.get("/burgers", function (req, res) {
+    burger.all(function (data) {
+        var hbsObject = {
+            burgers: data
+        };
+        res.render("index", hbsObject)
+    });
+});
+
+router.post("/api/burgers", function (req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.devoured
+    ], function (result) {
+        res.json({ id: result.insertId })
+    })
+})
+
+router.put("/api/burgers/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition, function (result) {
+        if (result.devoured == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+// post and put methods (delete not needed)
+// 13-MVC activity 17
+
+
+
+module.exports = router;
